e0b84175a12a22c808b0a6ba024634ccf54f5413
lllllLeo <kwb103@gmail.com>
2018-10-15 14:56:11 +0900

    초기설정

---
+++ src/main/java/main/Controller/MemberController/MemberController.java
@@ -0,0 +1,15 @@
+package main.Controller.MemberController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MemberController {
+
+    @RequestMapping(value = "")
+    public String hi(){
+
+        return "home";
+    }
+}
