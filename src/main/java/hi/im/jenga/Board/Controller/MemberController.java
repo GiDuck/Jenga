97bfe4818faa2ed6fbb57dc849a918e2b1658695
HoJun <ghwnswkd2002@naver.com>
2018-10-15 19:54:03 +0900

    package 정리

--- src/main/java/hi/im/jenga/Board/Controller/MemberController.java
+++ src/main/java/hi/im/jenga/Board/Controller/MemberController.java
@@ -1,4 +1,4 @@
-package main.Controller.MemberController;
+package hi.im.jenga.Board.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
