f0699b9c9c2e716763e992421c03f1738a99f282
lllllLeo <kwb103@gmail.com>
2018-11-06 11:17:16 +0900

    기덕과 합침완료

--- src/main/java/hi/im/jenga/Board/controller/BoardController.java
+++ src/main/java/hi/im/jenga/Board/controller/BoardController.java
@@ -1,4 +1,4 @@
-package hi.im.jenga.member.controller;
+package hi.im.jenga.board.controller;
 
 import hi.im.jenga.board.service.BoardService;
 import hi.im.jenga.board.service.MongoService;
@@ -9,20 +9,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 @RequestMapping("/board")
-public class BoardController2 {
-    private static final Logger logger= LoggerFactory.getLogger(BoardController2.class);
+public class BoardController {
+    private static final Logger logger= LoggerFactory.getLogger(BoardController.class);
     private final MongoService mongoService;
     private final BoardService boardService;
 
     @Autowired
-    public BoardController2(MongoService mongoService, BoardService boardService) {
+    public BoardController(MongoService mongoService, BoardService boardService) {
         this.mongoService = mongoService;
         this.boardService = boardService;
     }
-
     @RequestMapping(value="/stackBlock")
     public String getWriteView(Model model) {
 
@@ -34,11 +32,9 @@ public class BoardController2 {
         return "editor/stackBoard/stackBlock";
     }
 
-
     @RequestMapping(value = "/mongo")
     public String mongo(){
         mongoService.getAnyway();
         return "/mongo";
     }
-
 }
